8aca99b9a3a247be40ae17152c84da5a4ff6196a
Francisco <f.orchard@epiconcept.fr>
2020-05-22 13:23:43 +0200

    making package script to gerenarte both binaries and no binaty packages

--- devscripts/package.R
+++ devscripts/package.R
@@ -5,16 +5,19 @@ if(!("devtools" %in% installed.packages()[,"Package"]))
 
 devtools::document()
 devtools::build(binary=TRUE)
+devtools::build(binary=FALSE)
 
 if(!file.exists("install")){
   dir.create("install", showWarnings = FALSE)
 }
 
 installer_name <- (
-  if(.Platform$OS.type == "windows")
-    "epitweetr_0.0.0.9000.zip" 
-  else
-    "epitweetr_0.0.0.9000_R_x86_64-pc-linux-gnu.tar.gz" 
+  c("epitweetr_0.0.0.9000.tar.gz",
+    if(.Platform$OS.type == "windows")
+      "epitweetr_0.0.0.9000.zip" 
+    else
+      "epitweetr_0.0.0.9000_R_x86_64-pc-linux-gnu.tar.gz" 
+  )
 )
 file.rename(file.path("..", installer_name), file.path("..", "install", installer_name))
 
