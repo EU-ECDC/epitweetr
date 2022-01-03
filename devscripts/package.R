361f6907fac5c9675a960b49ef7a460a42b2f54e
forchard <forchard@protonmail.com>
2022-01-03 15:15:23 +0100

    improving package.R to avoid error due to choices of installation options

--- devscripts/package.R
+++ devscripts/package.R
@@ -6,8 +6,6 @@ if(!("devtools" %in% installed.packages()[,"Package"]))
 
 #devtools::install_deps()
 #devtools::document()
-devtools::build(binary=TRUE)
-devtools::build(binary=FALSE)
 # devtools::build_manual()
 # devtools::build_vignettes()
 
@@ -18,6 +16,18 @@ if(!file.exists(file.path("..", "manual"))){
   dir.create(file.path("..", "manual"), showWarnings = FALSE)
 }
 
+if(any(grepl("epitweetr.*", sessionInfo())))
+  detach("package:epitweetr", unload=TRUE)
+
+
+
+installer_path <- c(
+  devtools::build(binary=FALSE)
+  ,devtools::build(binary=TRUE)
+)
+install.packages(file.path(installer_path[[2]]), dependencies = FALSE)
+
+
 #moving installer
 installer_name <- (
   c(paste("epitweetr_",packageVersion("epitweetr"),".tar.gz", sep = ""),
@@ -27,13 +37,11 @@ installer_name <- (
       paste("epitweetr_",packageVersion("epitweetr"),"_R_x86_64-pc-linux-gnu.tar.gz", sep = "") 
   )
 )
-file.rename(file.path("..", installer_name), file.path("..", "install", installer_name))
+file.rename(file.path(installer_path), file.path("..", "install", installer_name))
 
 manual_name <- paste("epitweetr_",packageVersion("epitweetr"),".pdf", sep = "") 
 #moving manual
 #file.rename(file.path("..", manual_name), file.path("..", "manual", manual_name))
 
 
-#detach("package:epitweetr", unload=TRUE)
-#install.packages(file.path("..", "install", installer_name[[2]]), dependencies = TRUE)
 
